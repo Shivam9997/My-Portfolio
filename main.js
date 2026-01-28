b04282356b00fefd580496765a88420810502f1e
Shivam9997 <shivamrathore2805@gmail.com>
2026-01-29 01:02:50 +0530

    add new file

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings: ["Web Developer", "Frontend Designer", "UI/UX Enthusiast"],
+    typeSpeed:100,
+    backSpeed:60,
+    backDelay:1000,
+    loop:true
+})
